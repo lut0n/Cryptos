09ebf85cd7770ad9bcd3359ff507110bfcc32729
David Pikhunyk <101625277+lut0n@users.noreply.github.com>
2022-04-02 16:53:10 +0300

    Add files via upload

---
+++ Cryptos.py
@@ -0,0 +1,115 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+ 
+text = ''
+UseFile = False
+
+#Getting input
+def getTextInput():
+	userInput = TextInput.get()
+	return userInput
+def getkey():
+	userInput = Key.get()
+	return userInput
+#File system
+def File():
+    with open(getTextInput(), 'r') as f:
+        global text
+        global UseFile
+        UseFile = True
+        text = f.read()
+        f.close()
+def File_Of():
+    global text
+    global UseFile
+    UseFile = False
+    text = getTextInput()
+#Encoding funcitons
+def Encode_Com(msg):
+    key = getkey()
+    num = 1
+    counter = 0
+    c = 0
+    out = ''
+    for i in key:
+        num += int(i)
+    for i in msg:
+        if c == len(key):
+            c = 0
+        counter += 1
+        counter += int(key[c])
+        c += 1
+        out += chr(ord(i) + counter)
+    TextInput.delete('0', END)
+    global UseFile
+    if not UseFile:
+        TextInput.insert(tk.END, out)
+    if UseFile:
+        with open('Export.txt', 'w', encoding='utf-8') as f:
+            f.write(out)
+            f.close()
+def Encode():
+    Encode_Com(text)
+def Decode_Com(msg):
+    out = ''
+    key = getkey()
+    num = 1
+    counter = 0
+    c = 0
+    for i in key:
+        num += int(i)
+    for i in msg:
+        if c == len(key):
+            c = 0
+        counter += 1
+        counter += int(key[c])
+        out += chr(ord(i)- counter)
+        c += 1
+    TextInput.delete('0', END)
+    global UseFile
+    if not UseFile:
+        TextInput.insert(tk.END, out)
+    if UseFile:
+        with open('Export.txt', 'w', encodeing='utf-8') as f:
+            f.write(out)
+            f.close()
+def Decode():
+    Decode_Com(text)
+#Tkinter
+root = Tk()
+
+root.geometry('300x250')
+root.configure(background='#FFFFFF')
+root.title('Cryptos')
+
+Button(root, text='Decode', command=Decode).place(x=50, y=200)
+
+Button(root, text='Encode', command=Encode).place(x=150, y=200)
+
+Button(root, text='File', command=File).place(x=250, y=100)
+Button(root, text='File_Of', command=File_Of).place(x=250, y=150)
+
+Button(root, text='Exit', command=root.destroy).place(x=0, y=0)
+
+Label(root, text='Cryptos', bg='#FFFFFF').place(x=120, y=20)
+
+TextInput=Entry(root)
+TextInput.place(x=90, y=140)
+
+Key=Entry(root)
+Key.place(x=90, y=80)
+
+Label(root, text='key:', bg='#FFFFFF').place(x=120, y=50)
+
+Label(root, text='text:', bg='#FFFFFF').place(x=120, y=100)
+root.mainloop()
+
+
+
+
+
+
+
+
+
